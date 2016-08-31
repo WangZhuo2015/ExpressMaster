76b13f12b1f1cbcb51691a87b2c3c954c277c1e3
王卓 <280034816@qq.com>
2016-08-31 19:12:09 +0800

    init

---
+++ ExpressMaster/Tabbar/MainTabBarViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  CookBook
+//
+//  Created by 王卓 on 16/6/5.
+//  Copyright © 2016年 SherryTeam. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Rswift
+
+class MainTabbarController: UITabBarController {
+    
+    //    private var indexViewController:IndexViewController!
+    //    private var mineViewController:MineViewController!
+    
+    private var tabbarItems:[UITabBarItem] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadViewControllers()
+        configTabbar()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func loadViewControllers(){
+        func configViewController(storyBoard:UIStoryboard,title:String,attr:[String:UIColor],image:UIImage?,selectedImage:UIImage?) -> UINavigationController {
+            let viewController = storyBoard.instantiateInitialViewController() as! UINavigationController
+            viewController.tabBarItem.image = image
+            viewController.tabBarItem.selectedImage = selectedImage
+            viewController.navigationItem.title = title
+            viewController.tabBarItem.title = title
+            viewController.tabBarItem.setTitleTextAttributes(attr, forState: .Normal)
+            return viewController
+        }
+        
+        let color = [NSForegroundColorAttributeName:UIColor.whiteColor()]
+        let indexViewController = configViewController(R.storyboard.index(), title: "今日美食", attr: color, image: R.image.today_normal(), selectedImage: R.image.today_highlighted())
+        
+        let mineViewController = configViewController(R.storyboard.mine(), title: "我的", attr: color, image: R.image.user_normal(), selectedImage: R.image.user_highlighted())
+        
+        self.viewControllers = [
+            indexViewController,
+            mineViewController
+        ]
+        self.selectedIndex = 0
+    }
+    
+    func configTabbar(){
+        UITabBar.appearance().barTintColor = UIColor.darkGrayColor()
+        UITabBar.appearance().tintColor = UIColor.whiteColor()
+    }
+    
+}
